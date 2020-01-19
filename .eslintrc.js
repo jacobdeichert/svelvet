085d94847c5bd1dff6d83482634078016a76ee45
Jake Deichert <git@jakedeichert.com>
2020-01-19 01:17:49 -0500

    Fix lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,27 @@
+const OFF = 'off';
+const WARN = 'warn';
+const ERROR = 'error';
+
 module.exports = {
-    extends: ['@jakedeichert/eslint-config'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    extends: [
+        '@jakedeichert/eslint-config',
+        'plugin:@typescript-eslint/recommended',
+    ],
+    parserOptions: {
+        project: `${__dirname}/tsconfig.json`,
+    },
+
     env: {
         node: true,
+    },
+
+    rules: {
+        '@typescript-eslint/indent': OFF, // Prettier handles formatting
+        '@typescript-eslint/no-use-before-define': [
+            ERROR,
+            { functions: false },
+        ],
     },
 };
