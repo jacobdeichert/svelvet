f3308f82bcd068b047e487d9d3f89a779a390bd4
Jake Deichert <git@jakedeichert.com>
2020-01-18 22:11:39 -0500

    Add lint and formatting

---
+++ .eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  extends: ["@jakedeichert/eslint-config"],
+  env: {
+    node: true
+  }
+};
---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    tabWidth: 4,
+    singleQuote: true,
+    trailingComma: 'es5',
+    overrides: [
+        {
+            files: ['*.json'],
+            options: {
+                tabWidth: 2,
+            },
+        },
+    ],
+};
