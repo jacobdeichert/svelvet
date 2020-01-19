171fd4572335f66c28da11e84b75f9e6b54e4c46
Jake Deichert <git@jakedeichert.com>
2020-01-19 00:09:43 -0500

    Move tests to a snapshot dir

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,17 +2,17 @@
 
 set -e # Exit on error
 
-cd test
+cd tests/snapshot
 
 # Install and build the snapshot dist directory
 npm install
 npm run build:prod
 
 # Format dist so it's more readable
-cd ..
+cd ../..
 npm run format
 
-# Exit with an error if there's any changes in the test/dist directory
+# Exit with an error if there's any changes in the tests/snapshot/dist directory
 test_err_msg="\n\nTEST ERROR: dist contains changed files that do not match the snapshot\n\n"
 git add -A
-git diff-index --quiet HEAD -- test/dist || (printf "$test_err_msg" && git status && exit 1);
+git diff-index --quiet HEAD -- tests/snapshot/dist || (printf "$test_err_msg" && git status && exit 1);
