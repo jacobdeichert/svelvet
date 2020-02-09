a7ef68f27a6b8cf67a75f02a98f31df664cf7640
Jake Deichert <git@jakedeichert.com>
2020-02-09 18:20:15 -0500

    Fix snowpack babel error
    Snowpack released 1.2.0 which changed their babel plugin logic a bit. https://github.com/pikapkg/snowpack/pull/189/files#diff-dbcfdbbaf972e7b385fa6fcb270a59d9R60
    
    Since we use their babel plugin, this broke our babel transform step.
    
    Fixed by using the new `importMap` option and specifying where to look for it.

--- src/index.ts
+++ src/index.ts
@@ -25,6 +25,7 @@ const BABEL_CONFIG = existsSync('./babel.config.js')
                   {
                       // Append .js to all src file imports
                       optionalExtensions: true,
+                      importMap: '../dist/web_modules/import-map.json',
                   },
               ],
           ],
