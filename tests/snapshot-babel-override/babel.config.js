8a01e840a2342c084c8c9bdee87718b3051d466c
Jake Deichert <git@jakedeichert.com>
2020-03-16 23:38:29 -0400

    Fix example babel config

--- tests/snapshot-babel-override/babel.config.js
+++ tests/snapshot-babel-override/babel.config.js
@@ -6,7 +6,8 @@ module.exports = {
             {
                 // Append .js to all src file imports
                 optionalExtensions: true,
-                importMap: '../dist/web_modules/import-map.json',
+                dir: 'dist/web_modules',
+                importMap: '../../public/dist/web_modules/import-map.json',
             },
         ],
     ],
